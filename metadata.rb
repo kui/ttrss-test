905192c1b24adad8cf5ce61219c38e5b27269036
Keiichiro Ui <keiichiro.ui@gmail.com>
2013-07-23 03:00:35 +0900

    init

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'ttrss-test'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures ttrss-test'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
